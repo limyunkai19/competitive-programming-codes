033a828ddf4466abbf897e9b01a38acd3d4915ed
limyunkai19 <limyunkai19@gmail.com>
2022-03-13 19:22:32 +0800

    feat: add snippet for C++ faster IO

---
+++ Data Structure and Algorithm Rev 1/fast_io.cpp
@@ -0,0 +1,15 @@
+// use the following code for faster IO in C++
+// helps when the time limit is tight
+//
+// note: the code is proven faster than scanf/printf
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    return 0;
+}
